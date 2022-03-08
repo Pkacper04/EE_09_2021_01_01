a0c652de662f09c861bf84325638afcdc429796f
Pkacper04 <kacperpotaczala001@gmail.com>
2022-03-08 10:46:45 +0100

    smallChanges

--- futbol.php
+++ futbol.php
@@ -2,12 +2,37 @@
 <html>
 <head>
     <meta charset = "UTF-8">
-    <link rel="Styelsheet" href="">
+    <title>Rozgrywki futbolowe</title>
+    <link rel="stylesheet" href="styl.css">
 </head>
 <body>
+<div id="baner">
+    <h2>Światowe rozgrywki piłkarskie</h2>
+    <img src="obraz1.jpg" alt="boisko">
+</div>
 
-df
+<div id="mecze">
+</div>
 
+<div id="glowny">
+    <h2>Reprezentacja Polski</h2>
+</div>
+
+<div id="lewy">
+    <p>Podaj pozycję zawodników (1-bramkarze,  2-obrońcy, 3-pomocnicy, 4-napastnicy):</p>
+    <form action="futbol.php" method="POST">
+        <input type="number" id="pozycja">
+        <input type="submit" value="Sprawdź">
+    </form>
+    <ul>
+
+    </ul>
+</div>
+
+<div id="prawy">
+    <img src="zad1.png" alt="piłkarz">
+    <p>Autor: Kacper Potaczała</p>
+</div>
 </body>
 </html>
 
